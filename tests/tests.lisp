adaf6ba1b01febb954954e33d1b1a05a3a110677
Jean-Philippe Paradis <hexstream@hexstreamsoft.com>
2018-09-25 09:49:55 -0400

    Tests: Use proper argument order for IS.

--- tests/tests.lisp
+++ tests/tests.lisp
@@ -5,12 +5,11 @@
 (cl:in-package #:macro-level_tests)
 
 (define-test "main"
-  (is equal
+  (is equal '((*a* . 1) (*b* . 2) (*c* . 3))
       (let ((alist nil))
         (macro-level `(progn ,@(mapcar (lambda (var value)
                                          (let ((var (intern (format nil "*~A*" var) '#:macro-level_tests)))
                                            `(push '(,var . ,value) alist)))
                                        '(a b c)
                                        '(1 2 3))))
-        (nreverse alist))
-      '((*a* . 1) (*b* . 2) (*c* . 3))))
+        (nreverse alist))))
